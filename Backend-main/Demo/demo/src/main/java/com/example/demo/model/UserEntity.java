b75e875c4d92f8ebe72c2ae0c2fd653a45600dcb
SOOMIN213 <163002349+SOOMIN213@users.noreply.github.com>
2024-03-11 22:26:51 +0900

    Add files via upload

---
+++ Backend-main/Demo/demo/src/main/java/com/example/demo/model/UserEntity.java
@@ -0,0 +1,37 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+public class UserEntity {
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy = "uuid")
+	private String id; 
+
+	@Column(nullable = false)
+	private String username; 
+
+	@Column(nullable = false)
+	private String email; 
+
+	@Column(nullable = false)
+	private String password; 
+}
